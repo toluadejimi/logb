a82a93838307094a852b7c1455d2354f4de5a224
Jimmy <toluadejimi@gmail.com>
2023-10-15 13:24:10 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// 1. https://api.telegram.org/bot6672089802:AAElshUyomrixNlnmiJNb7g75v9ku5YG3zc/getUpdates
+// 2. https://api.telegram.org/bot6672089802:AAElshUyomrixNlnmiJNb7g75v9ku5YG3zc/setWebhook?url=https://0a27-102-89-45-117.ngrok-free.app/api/webhook
+// 3. https://api.telegram.org/bot<Your-token>/getWebhookInfo
+// 4. https://api.telegram.org/bot<Your-token>/deleteWebhook
